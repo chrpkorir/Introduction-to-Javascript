cb937d18ab6ba0bba4164518ab193a179d102535
[chrpkorir] <faythkoko@gmail.com>
2022-06-23 10:32:14 +0300

    Js intro

---
+++ index.js
@@ -0,0 +1,7 @@
+var myname = "Faith";
+var height = 21;
+var country = "Kenya";
+
+console.log("My name is " + myname);
+console.log("My height is " + height);
+console.log("My country is " + country);
